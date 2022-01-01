de3cfdfd359591afe6c8238f86a23cf4c04125b4
Islam <islamibakaev@gmail.com>
2022-01-01 18:37:33 +0300

    getting started with styles

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{padding: 16}}>
+      <View>
+        <TextInput placeholder="Course Goal" />
+        <Button title="Add" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
