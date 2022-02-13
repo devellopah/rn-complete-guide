60373007ffa978dfaab0a6b44dacf8ac83e243c3
Islam <islamibakaev@gmail.com>
2022-02-13 19:19:28 +0300

    add Header

--- App.js
+++ App.js
@@ -1,57 +1,19 @@
-import { useState } from 'react';
-import { StyleSheet, View, FlatList, Button } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Header from './components/Header'
 
-import GoalItem from './components/GoalItem';
-import GoalInput from './components/GoalInput';
-
-export default function App() {
-  const [goals, setGoals] = useState([])
-  const [isAddMode, setAddMode] = useState(false)
-
-  const addGoal = (goal) => {
-    setGoals(currentGoals => [
-      ...currentGoals,
-      { key: Math.random().toString(), value: goal }
-    ])
-
-    setAddMode(false)
-  }
-
-  const deleteGoal = (goalId) => {
-    setGoals(currentGoals =>
-      currentGoals.filter(item => item.key !== goalId)
-    )
-  }
-
-
-  const cancelGoalAddition = () => {
-    setAddMode(false)
-  }
-
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Button
-        title="Add goal"
-        onPress={setAddMode.bind(this, true)}
-      />
-      <GoalInput
-        addGoal={addGoal}
-        onCancel={cancelGoalAddition}
-        isModalVisible={isAddMode}
-      />
-      <FlatList data={goals} renderItem={text =>
-        <GoalItem
-          title={text.item.value}
-          deleteItem={deleteGoal.bind(this, text.item.key)}
-        />
-      }>
-      </FlatList>
+    <View style={styles.screen}>
+      <Header title="Guess a Number" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 48,
-  },
+  screen: {
+    flex: 1,
+  }
 });
+
+export default App
